25a5393e511e19268b175fe7f6913720e21863b4
AlekseySapi <altmind92@gmail.com>
2023-05-02 19:39:40 +0300

    Deleted menu_main

--- app/src/main/java/ru/alekseysapi/materialyou/ui/MainActivity.kt
+++ app/src/main/java/ru/alekseysapi/materialyou/ui/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.geekbrains.materialyou.R
 import com.geekbrains.materialyou.databinding.ActivityMainBinding
 import com.geekbrains.materialyou.ui.picture.PictureOfTheDayFragment
-import ru.alekseysapi.materialyou.R
-import ru.alekseysapi.materialyou.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
+        //setTheme(R.style.PinkTheme)
         setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
